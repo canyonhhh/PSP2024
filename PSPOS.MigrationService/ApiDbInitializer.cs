541dd13087ab66e757e26e11a182f2b4b72e1bb4
Joris <34213695+canyonhhh@users.noreply.github.com>
2024-12-08 12:37:57 +0200

    add editorconfig and github action (#6)

--- PSPOS.MigrationService/ApiDbInitializer.cs
+++ PSPOS.MigrationService/ApiDbInitializer.cs
@@ -1,11 +1,11 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
-using System.Diagnostics;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Storage;
 using PSPOS.ApiService.Data;
+using System.Diagnostics;
 
 namespace PSPOS.MigrationService;
 
