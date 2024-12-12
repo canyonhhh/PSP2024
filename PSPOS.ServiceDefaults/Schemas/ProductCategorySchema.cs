6a59b3f724f989aa865583f0a9713ee96cc189c8
Cioperis <118674199+Cioperis@users.noreply.github.com>
2024-12-12 19:53:14 +0200

    fixed required properties

--- PSPOS.ServiceDefaults/Schemas/ProductCategorySchema.cs
+++ PSPOS.ServiceDefaults/Schemas/ProductCategorySchema.cs
@@ -14,7 +14,7 @@ namespace PSPOS.ServiceDefaults.Schemas
         public DateTime? updatedAt { get; set; }
         public Guid? createdBy { get; set; }
         public Guid? updatedBy { get; set; }
-        public string name { get; set; }
+        public required string name { get; set; }
         public string? description { get; set; }
         public Guid[] productOrServiceIds { get; set; }
     }
