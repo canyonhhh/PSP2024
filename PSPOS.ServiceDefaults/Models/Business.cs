b09af4fba06f4efa26a177744da1f0cd7e81f921
Canyon <jorislisas@gmail.com>
2024-12-20 15:24:03 +0200

    validation

--- PSPOS.ServiceDefaults/Models/Business.cs
+++ PSPOS.ServiceDefaults/Models/Business.cs
@@ -1,3 +1,4 @@
+using System.ComponentModel.DataAnnotations;
 using System.Runtime.InteropServices.JavaScript;
 
 namespace PSPOS.ServiceDefaults.Models;
@@ -6,7 +7,9 @@ public class Business(string name, string phone, string email, Currency defaultC
     : BaseClass
 {
     public string Name { get; set; } = name;
+    [Phone(ErrorMessage = "Invalid phone number format.")]
     public string Phone { get; set; } = phone;
+    [EmailAddress(ErrorMessage = "Invalid email format.")]
     public string Email { get; set; } = email;
     public Currency DefaultCurrency { get; set; } = defaultCurrency;
     public bool IsInitialized { get; set; } = false;
